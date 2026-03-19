caf4e21b928e0bc87477015f1b4563159085bcc2
leehyoeun <s25049@gsm.hs.kr>
2026-03-19 21:08:53 +0900

    feat: 헤더 컴포넌트 추가

---
+++ src/widget/header/ui/headerMenu.tsx
@@ -0,0 +1,22 @@
+"use client";
+import Link from "next/link";
+
+interface HeaderMenuProps {
+  title: string;
+  isActive: boolean;
+  href: string;
+}
+
+export function HeaderMenu({ title, isActive, href }: HeaderMenuProps) {
+  return (
+    <Link href={href}>
+      <span
+        className={`text-[20px] font-medium 
+          ${isActive ? "text-main" : "text-black group-hover:text-main"}
+        `}
+      >
+        {title}
+      </span>
+    </Link>
+  );
+}
---
+++ src/widget/header/ui/index.tsx
@@ -0,0 +1,36 @@
+"use client";
+
+import Link from "next/link";
+import Logo from "@/src/shared/asset/img/gif_logo.png";
+import Image from "next/image";
+import { HeaderMenu } from "./headerMenu";
+import { usePathname } from "next/navigation";
+
+export default function Header() {
+  const pathname = usePathname();
+  // 주소가 아직 정해지지 않아 비워 둠
+  const menuList = [
+    { title: "프로젝트 확인", href: "/"},
+    { title: "양식 생성", href: ""},
+    { title: "제출 확인", href: ""},
+    { title: "점수 수합 확인", href: ""},
+  ]
+  return (
+    <header className="h-20 w-full flex justify-center items-center border-b border-gray-70 bg-white">
+      <Link href="/">
+        <Image src={Logo} alt="홈으로 이동" width={61} height={56} priority />
+      </Link>
+
+      <div className="flex gap-6 ml-[70px]">
+        {menuList.map((menu) => (
+          <HeaderMenu 
+            key={menu.title} 
+            title={menu.title}
+            isActive={pathname === menu.href}
+            href={menu.href}
+          />
+        ))}
+      </div>
+    </header>
+  );
+}
