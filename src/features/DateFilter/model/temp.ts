2f44daa71b88e5008751ed5e312755b3ca77f3a1
ma94275 <s25020@gsm.hs.kr>
2026-03-23 16:16:57 +0900

    feat: 제출 목록 임시데이터 생성

---
+++ src/features/DateFilter/model/temp.ts
@@ -0,0 +1,44 @@
+export const MOCK_SUBMISSION = [
+  {
+    id: 1,
+    title: '배너 제출하기',
+    dateStart: '2026-03-04',
+    dateEnd: '2026-03-05',
+    grade: 1 as const,
+  },
+  {
+    id: 2,
+    title: '팀 로고 제출하기',
+    dateStart: '2026-05-11',
+    dateEnd: '2026-05-18',
+    grade: 1 as const,
+  },
+  {
+    id: 3,
+    title: '팀 로고 제출하기',
+    dateStart: '2026-05-03',
+    dateEnd: '2026-05-05',
+    grade: 2 as const,
+  },
+  {
+    id: 4,
+    title: '포스터 제출하기',
+    dateStart: '2026-10-25',
+    dateEnd: '2026-10-26',
+    grade: 2 as const,
+  },
+  {
+    id: 5,
+    title: '보고서 제출하기',
+    dateStart: '2026-03-15',
+    dateEnd: '2026-03-19',
+    grade: 1 as const,
+  },
+  {
+    id: 6,
+    title: '포스터 제출하기',
+    dateStart: '2025-10-25',
+    dateEnd: '2025-10-26',
+    grade: 1 as const,
+  },
+];
