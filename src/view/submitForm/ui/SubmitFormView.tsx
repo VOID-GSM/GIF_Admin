198fb509dc062a6027bb670f39fc71f37aec6b32
ma94275 <s25020@gsm.hs.kr>
2026-03-23 20:02:54 +0900

    refactor: 학년 매직넘버 상수로 분리

--- src/view/submitForm/ui/SubmitFormView.tsx
+++ src/view/submitForm/ui/SubmitFormView.tsx
@@ -6,16 +6,16 @@ import DateFilter from '@/src/features/DateFilter/ui/DateFilter';
 
 export default function SubmitForm() {
   const [selectedGrade, setSelectedGrade] = useState(1);
-  const grades = [1, 2];
+  const SUPPORTED_GRADES: number[] = [1, 2];
 
   return (
     <div className="flex flex-col gap-[50px] items-center mt-20">
-      <div className="flex gap-25">
-        {grades.map((grade) => (
+      <div className="flex gap-[100px]">
+        {SUPPORTED_GRADES.map((grade) => (
           <GradeButton
             key={grade}
             value={grade}
-            isSeleted={selectedGrade === grade}
+            isSelected={selectedGrade === grade}
             onClick={() => setSelectedGrade(grade)}
           />
         ))}
