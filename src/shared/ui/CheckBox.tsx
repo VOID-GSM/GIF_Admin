b807cecb2fc4842ee9b91d2b7a0b4693dd39eec5
jyuuuuu0 <s25023@gsm.hs.kr>
2026-03-24 18:27:31 +0900

    feat: CheckBox 아이콘 생성

---
+++ src/shared/ui/CheckBox.tsx
@@ -0,0 +1,34 @@
+export default function CheckBox({ isActive = true }) {
+  return (
+    <>
+      {isActive ? (
+        <svg
+          width="17"
+          height="17"
+          viewBox="0 0 17 17"
+          fill="none"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <rect x="0.5" y="0.5" width="16" height="16" rx="1.5" fill="#A7A7A7" stroke="#A7A7A7" />
+          <path
+            d="M4 7.73913L7.78378 12L14 5"
+            stroke="white"
+            stroke-width="1.5"
+            stroke-linecap="round"
+            stroke-linejoin="round"
+          />
+        </svg>
+      ) : (
+        <svg
+          width="17"
+          height="17"
+          viewBox="0 0 17 17"
+          fill="none"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <rect x="0.5" y="0.5" width="16" height="16" rx="1.5" stroke="#A7A7A7" />
+        </svg>
+      )}
+    </>
+  );
+}
