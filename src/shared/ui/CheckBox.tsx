e744a42c967cc17330a44ed8ff8eba4eb2c1b066
jyuuuuu0 <s25023@gsm.hs.kr>
2026-03-24 19:55:41 +0900

    fix: svg 카멜 케이스로 수정

--- src/shared/ui/CheckBox.tsx
+++ src/shared/ui/CheckBox.tsx
@@ -13,9 +13,9 @@ export default function CheckBox({ isActive = true }) {
           <path
             d="M4 7.73913L7.78378 12L14 5"
             stroke="white"
-            stroke-width="1.5"
-            stroke-linecap="round"
-            stroke-linejoin="round"
+            strokeWidth="1.5"
+            strokeLinecap="round"
+            strokeLinejoin="round"
           />
         </svg>
       ) : (
