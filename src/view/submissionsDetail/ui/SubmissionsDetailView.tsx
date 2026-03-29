434fd962bb12d3f82e087ebcfee54e33c15307a3
ma94275 <s25020@gsm.hs.kr>
2026-03-30 01:26:24 +0900

    feat:  상세 확인 페이지 추가

---
+++ src/view/submissionsDetail/ui/SubmissionsDetailView.tsx
@@ -0,0 +1,21 @@
+import SubmissionsCard from '@/features/Detailsubmissions/SubmissionsCard';
+import SubmissionsGuideCard from '@/features/Detailsubmissions/ui/SubmissionsGuideCard';
+import Card from '@/shared/ui/card/Card';
+import { MOCK_DETAIL } from '@/entities/detail/model/temp';
+
+export default function SubmissionsDetail() {
+  return (
+    <div className="flex flex-col items-center mt-24 mb-24">
+      <div className="flex flex-col gap-[30px] items-center">
+        <span className="text-2xl font-semibold">배너 작성 안내</span>
+        <span className="self-start font-medium text-gray-40">마감일: 2026.02.12</span>
+        {MOCK_DETAIL.map((item) => (
+          <Card key={item.id} className="flex-col gap-[20px]">
+            <SubmissionsGuideCard subtitle={item.subtitle} content={item.content} />
+            <SubmissionsCard item={item} />
+          </Card>
+        ))}
+      </div>
+    </div>
+  );
+}
