da74988405ea0d46171c9f9c663b60e58741b374
leehyoeun <s25049@gsm.hs.kr>
2026-03-30 00:26:38 +0900

    feat: Project 타입 정의 및 목 데이터 추가

---
+++ src/entities/main/model/mock.ts
@@ -0,0 +1,24 @@
+import { Project } from './type';
+
+export const MOCK_PROJECT: Project[] = [
+  {
+    id: '1',
+    title: 'PolishMe',
+    members: '김철수 외 2인',
+    imageUrl: '',
+    grade: 1,
+  },
+  {
+    id: '2',
+    title: 'PolishMe',
+    members: '김철수 외 2인',
+    imageUrl: '',
+    grade: 1,
+  },
+  { id: '3', title: 'PolishMe', members: '김철수 외 2인', imageUrl: '', grade: 1 },
+  { id: '4', title: 'PolishMe', members: '김철수 외 2인', imageUrl: '', grade: 2 },
+  { id: '5', title: 'PolishMe', members: '김철수 외 2인', imageUrl: '', grade: 2 },
+  { id: '6', title: 'PolishMe', members: '김철수 외 2인', imageUrl: '', grade: 2 },
+  { id: '7', title: 'PolishMe', members: '김철수 외 2인', imageUrl: '', grade: 1 },
+  { id: '8', title: 'PolishMe', members: '김철수 외 2인', imageUrl: '', grade: 1 },
+];
