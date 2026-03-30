ea49c183d72e171a324b45b47785b08a7a5aab7d
ma94275 <s25020@gsm.hs.kr>
2026-03-31 01:26:25 +0900

    feat: 화살표, 삭제 svg 생성

---
+++ src/shared/asset/svg/arrow.tsx
@@ -0,0 +1,26 @@
+interface CalenderProps {
+  isOpen: boolean;
+}
+
+export default function Arrow({ isOpen }: CalenderProps) {
+  return (
+    <div>
+      <svg
+        width="20"
+        height="11"
+        viewBox="0 0 20 11"
+        fill="none"
+        xmlns="http://www.w3.org/2000/svg"
+        className={`transition-transform duration-200 ${isOpen ? 'rotate-180' : 'rotate-0'}`}
+      >
+        <path
+          d="M1 1L10 10L19 1"
+          stroke="#C8C8C8"
+          strokeWidth="2"
+          strokeLinecap="round"
+          strokeLinejoin="round"
+        />
+      </svg>
+    </div>
+  );
+}
