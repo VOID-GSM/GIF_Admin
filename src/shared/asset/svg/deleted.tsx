887f48d39b9454e85c86dbdb53c145e5da06c94a
ma94275 <s25020@gsm.hs.kr>
2026-03-31 20:17:59 +0900

    refactor: svg 클릭요소 button으로 접근성 개선

--- src/shared/asset/svg/deleted.tsx
+++ src/shared/asset/svg/deleted.tsx
@@ -5,17 +5,17 @@ interface DeletedProps {
 
 export default function Deleted({ className, onClick }: DeletedProps) {
   return (
-    <svg
-      width="11"
-      height="11"
-      viewBox="0 0 11 11"
-      fill="none"
-      xmlns="http://www.w3.org/2000/svg"
-      className={className}
-      onClick={onClick}
-    >
-      <path d="M0.696136 0.5L10.5001 10.5" stroke="black" strokeLinecap="round" />
-      <path d="M10.3039 0.5L0.500003 10.5" stroke="black" strokeLinecap="round" />
-    </svg>
+    <button className={className} onClick={onClick}>
+      <svg
+        width="11"
+        height="11"
+        viewBox="0 0 11 11"
+        fill="none"
+        xmlns="http://www.w3.org/2000/svg"
+      >
+        <path d="M0.696136 0.5L10.5001 10.5" stroke="black" strokeLinecap="round" />
+        <path d="M10.3039 0.5L0.500003 10.5" stroke="black" strokeLinecap="round" />
+      </svg>
+    </button>
   );
 }
