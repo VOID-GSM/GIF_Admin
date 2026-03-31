ba020ffb205ace511c38b5a25b3f0b8d35eb716d
leehyoeun <s25049@gsm.hs.kr>
2026-04-01 01:30:59 +0900

    fix: admin 메뉴 내 등수 확인 페이지 /scores로 경로 수정

--- src/widget/header/model/menuData.ts
+++ src/widget/header/model/menuData.ts
@@ -6,7 +6,7 @@ export interface MenuItem {
 
 export const ADMIN_MENU: MenuItem[] = [
   { title: "프로젝트 확인", href: "/"},
-  { title: "등수 확인", href: "/ranking", isLockable: true},
+  { title: "등수 확인", href: "/scores", isLockable: true},
 ];
 
 export const MASTER_MENU: MenuItem[] = [
