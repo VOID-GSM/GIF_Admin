8d722d7dcad5dd84930f340c28efc328dcc90860
jyuuuuu0 <s25023@gsm.hs.kr>
2026-04-11 21:44:15 +0900

    fix: import 경로 수정

--- src/app/(master)/submissions/page.tsx
+++ src/app/(master)/submissions/page.tsx
@@ -1,4 +1,4 @@
-import Submissions from '@//view/submissions/ui/SubmissionsView';
+import Submissions from '@/view/submissions/ui/SubmissionsView';
 
 export default function SubmissionsPage() {
   return <Submissions />;
