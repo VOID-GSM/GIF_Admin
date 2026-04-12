ac60a2c7b5002338f1a4af0a61c9a9685f6b089f
jyuuuuu0 <s25023@gsm.hs.kr>
2026-04-13 08:20:37 +0900

    refactor: 뷰포트 단위 변경

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -2,7 +2,7 @@ import Link from 'next/link';
 
 export default function NotFound() {
   return (
-    <div className="w-full min-h-[calc(100vh-80px)] flex items-center justify-center flex-col">
+    <div className="w-full min-h-[calc(100dvh-80px)] flex items-center justify-center flex-col">
       <p className="font-decobox text-main text-6xl md:text-[80px]">404 ERROR</p>
       <p>죄송합니다. 페이지를 찾을 수 없습니다.</p>
       <Link href="/" className="font-medium text-main underline mt-11">
