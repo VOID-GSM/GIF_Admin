1639e8f1b958ebf49fed20d591b81372d0ff2729
jyuuuuu0 <s25023@gsm.hs.kr>
2026-04-12 19:13:49 +0900

    feat: 반응형 적용

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -3,7 +3,7 @@ import Link from 'next/link';
 export default function NotFound() {
   return (
     <div className="w-full min-h-[calc(100vh-80px)] flex items-center justify-center flex-col">
-      <p className="font-family-decobox text-main text-[80px]">404 ERROR</p>
+      <p className="font-family-decobox text-main text-6xl md:text-[80px]">404 ERROR</p>
       <p>죄송합니다. 페이지를 찾을 수 없습니다.</p>
       <Link href="/" className="font-medium text-main underline mt-11">
         홈으로 돌아가기
